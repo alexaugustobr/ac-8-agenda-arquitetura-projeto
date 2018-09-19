d789d9ee1cb52d08378b55ddd26409041f32d2d4
GabrielSBrito <31037869+GabrielSBrito@users.noreply.github.com>
2018-09-19 20:20:18 -0300

    Create ContatoDAO.py
    Add o arquivo ContatoDAO.py

---
+++ ContatoDAO.py
@@ -0,0 +1,17 @@
+class ContatoDAO:
+    def __init__(self):
+        self.contatos = []
+        print("Começando!!")
+    
+    def buscar(self, nome):
+        resultado = []
+        for contato in self.contatos:
+            if nome == contato.nome:
+                resultado.append(contato)
+        return resultado
+    
+    def inserir(self, contato):
+        self.contatos.append(contato)
+
+    def existe(self,contato):
+        return self.contatos.__contains__(contato)
